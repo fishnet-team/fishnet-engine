6b894a46b63013c349ecaa79e85d8ca16081a68d
Andrew Ishutin <hazmozavr@gmail.com>
2019-05-08 11:40:04 +0300

    Reset button added

--- server.py
+++ server.py
@@ -13,14 +13,8 @@ board = chess.Board()
 @app.route('/reset')
 def reset_board():
 	board.reset()
+	print('RESET')
 	return 'for the emperor'
-
-'''@app.route('/move/{}')
-def add_move(move):
-	print('got move: ' + move)
-	board.push(move)
-	return 'done'
-'''
 
 @app.route('/')
 def index():
@@ -41,18 +35,12 @@ def css_chessboard(data):
 
 @app.route('/get_move', methods=['POST'])
 def get_move():
-	#print(request.args)
-	#print(request.data)
-	#print(request.data.decode('ascii'))
 	s = request.data.decode('ascii')
-	#print(s, type(s))
 	fen = json.loads(s)
-	#print(fen)
 	board = chess.Board(fen['fen'])
 	move = engine.get_move(board)
 	print('move', move)
 	board.push(move)
 	move = str(move)
-	#move = move[:2] + '-' + move[2:]
 	
 	return move
